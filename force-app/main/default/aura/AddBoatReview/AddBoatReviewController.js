5deed1284b2b65eb6909f46ba68ed7a6f14b951a
Alejandro <alejandro.lorenzo.castellanos.contractor@bbva.com>
2020-07-03 12:49:33 +0200

    improved code

--- force-app/main/default/aura/AddBoatReview/AddBoatReviewController.js
+++ force-app/main/default/aura/AddBoatReview/AddBoatReviewController.js
@@ -7,47 +7,36 @@
     onSave : function(component, event, helper) {
         component.find("service").saveRecord(function(saveResult) {
             if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-                
-                // record is saved successfully
-                var resultsToast = $A.get("e.force:showToast");
-                if(resultsToast) {
-                    resultsToast.setParams({
+                var result = $A.get("e.force:showToast");
+                if(result) {
+                    result.setParams({
                         "title": "Saved",
                         "message": "The record was saved."
                     });
-                    resultsToast.fire();
+                    result.fire();
                 } else {
                     alert("The record was saved");
                 }            
                 var newBoatReviewAddedEvent = component.getEvent("BoatReviewAdded");
                 newBoatReviewAddedEvent.fire();
                 helper.onInit(component,event,helper);
-            } else if (saveResult.state === "INCOMPLETE") {
-                // handle the incomplete state
-                console.log("User is offline, device doesn't support drafts.");
-            } else if (saveResult.state === "ERROR") {
-                // handle the error state
-                console.log('Problem saving contact, error: ' + 
-                            JSON.stringify(saveResult.error));
             } else {
-                console.log('Unknown problem, state: ' + saveResult.state +
+                console.log('Error ocurred: state: ' + saveResult.state +
                             ', error: ' + JSON.stringify(saveResult.error));
             }
         });
-        
-        
     },
     
     onRecordUpdated : function (component, event , helper ) {
         var eventParams = event.getParams();
         if(eventParams.changeType === "CHANGED") {
-            var resultsToast = $A.get("e.force:showToast");
-            if(resultsToast) {
-                resultsToast.setParams({
+            var result = $A.get("e.force:showToast");
+            if(result) {
+                result.setParams({
                     "title": "Saved",
                     "message": "The record was saved."
                 });
-                resultsToast.fire();
+                result.fire();
             } else {
                 alert("The record was saved");
             }
